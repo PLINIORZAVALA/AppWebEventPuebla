8cb450896797b403b10ef8ab945714687853e8af
PLINIORZAVALA <134644204+PLINIORZAVALA@users.noreply.github.com>
2024-11-05 23:55:10 -0500

    Update consult data base and lis imagenes whit there description low and table

--- F_especiales.php
+++ F_especiales.php
@@ -19,19 +19,105 @@
         <li><a href="F_deporte.php"><span>Deportes</span></a></li>
         <li><a href="F_familiares.php"><span>Familiares</span></a></li>
         <li><a href="F_especiales.php" class="current"><span>Especiales</span></a></li>
-        <li><a href="#"><span>Todos</span></a></li>
+        <li><a href="F_todos.php"><span>Todos</span></a></li>
       </ul>
     </div>
   </div>
   <div id="container">
     <div id="content">
       <h3>Especiales</h3>
-      <p>Elaborador por:</p>
-      <p>Pedro Bello L&oacute;pez</p>
-      <p>pedro.bello@correo.buap.mx</p>
       <p>&nbsp;</p>
-      <p>&nbsp;</p>
-      <p>Que disfrutes de las peliculas...</p>
+      <p></p>
+
+      <?php
+        $link = mysqli_connect("localhost", "root", "", "event");
+
+        if (!$link) {
+            die('Error de conexión: ' . mysqli_connect_error());
+        }
+
+        $resultado = mysqli_query($link, "SELECT * FROM eventos");
+        if (!$resultado) {
+            die('Error en la consulta: ' . mysqli_error($link));
+        }
+
+        // Mostrar datos en una tabla
+        echo "<table border='1'>";
+        echo "<tr>
+                <th>ID Evento</th>
+                <th>Título</th>
+                <th>Descripción</th>
+                <th>Fecha del Evento</th>
+                <th>Hora del Evento</th>
+                <th>Ubicación</th>
+                <th>ID Usuario</th>
+                <th>Estado</th>
+                <th>Fecha de Creación</th>
+                <th>Tipo de Evento</th>
+                <th>Imagen</th>
+              </tr>";
+
+        while ($ren = mysqli_fetch_array($resultado)) {
+            $id_event = $ren['id_event'];
+            $titulo = $ren['titulo'];
+            $descripcion = $ren['descripcion'];
+            $fecha_evento = $ren['fecha_evento'];
+            $hora_evento = $ren['hora_evento'];
+            $ubicacion = $ren['ubicacion'];
+            $id_usuario = $ren['id_usuario'];
+            $estado = $ren['estado'];
+            $fecha_creacion = $ren['fecha_creacion'];
+            $tipo_event = $ren['tipo_event'];
+            $imagen = $ren['imagen'];
+
+            echo "<tr>";
+            echo "<td>$id_event</td>
+                  <td>$titulo</td>
+                  <td>$descripcion</td>
+                  <td>$fecha_evento</td>
+                  <td>$hora_evento</td>
+                  <td>$ubicacion</td>
+                  <td>$id_usuario</td>
+                  <td>$estado</td>
+                  <td>$fecha_creacion</td>
+                  <td>$tipo_event</td>";
+            echo "<td><a href='detalle_evento.php?id_event=$id_event'><img src='MisImagenes/$imagen' width='70' height='70'></a></td>";
+            echo "</tr>";
+        }
+
+        echo "</table>";
+
+        // Mostrar los datos en formato de tarjeta
+        echo "<h2>Vista de Tarjetas de Eventos</h2>";
+
+        mysqli_data_seek($resultado, 0); // Reiniciar puntero para reutilizar el resultado en el formato de tarjetas
+
+        while ($ren = mysqli_fetch_array($resultado)) {
+            $id_event = $ren['id_event'];
+            $titulo = $ren['titulo'];
+            $descripcion = $ren['descripcion'];
+            $fecha_evento = $ren['fecha_evento'];
+            $hora_evento = $ren['hora_evento'];
+            $ubicacion = $ren['ubicacion'];
+            $imagen = $ren['imagen'];
+            
+            echo "<div class='event-card' style='border: 1px solid #ddd; padding: 10px; width: 200px; margin: 10px; display: inline-block; text-align: center;'>";
+            echo "<img src='MisImagenes/$imagen' alt='Imagen del evento' style='width: 100%; height: auto;'>";
+            echo "<div class='event-info'>";
+            echo "<p class='event-title' style='font-weight: bold;'>$titulo</p>";
+            echo "<p class='event-description'>$descripcion</p>";
+            echo "<p class='event-date'>$fecha_evento a las $hora_evento</p>";
+            echo "<p class='event-location'>Ubicación: $ubicacion</p>";
+            echo "<p class='event-category'>Categoría: $tipo_event</p>";
+            echo "</div>";
+            echo "</div>";
+        }
+
+        mysqli_close($link);
+      ?>
+
+
+
       <p>&nbsp;</p>
       <p>&nbsp;</p>
       <p>&nbsp;</p>
--- F_todos.php
+++ F_todos.php
@@ -19,7 +19,7 @@
         <li><a href="F_deporte.php"><span>Deportes</span></a></li>
         <li><a href="F_familiares.php" class="current"><span>Familiares</span></a></li>
         <li><a href="F_especiales.php"><span>Especiales</span></a></li>
-        <li><a href="F_todos.php"><span>Todos</span></a></li>
+        <li><a href="F_todos.php" class="current"><span>Todos</span></a></li>
       </ul>
     </div>
   </div>
